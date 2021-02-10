90a7fdde29154e15a170269c27101829e2ba09d4
William Anderson <willjatlas@googlemail.com>
2021-02-10 16:38:10 +0000

    init

---
+++ src/main/java/com/example/codeclan/whiskytracker/WhiskytrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.whiskytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskytrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskytrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeclan/whiskytracker/WhiskytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.whiskytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskytrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
